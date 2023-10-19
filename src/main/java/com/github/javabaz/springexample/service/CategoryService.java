43b7339792cd553c7bc205177a6b64ad6b53c2a2
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:25:28 +0330

    add findByName method in CategoryService.

--- src/main/java/com/github/javabaz/springexample/service/CategoryService.java
+++ src/main/java/com/github/javabaz/springexample/service/CategoryService.java
@@ -1,4 +1,14 @@
 package com.github.javabaz.springexample.service;
 
+import com.github.javabaz.springexample.entity.Category;
+import com.github.javabaz.springexample.exception.CategoryNotFoundException;
+
+import java.util.List;
+
+
 public interface CategoryService {
+
+    Category findByName(String name) throws CategoryNotFoundException;
+
+
 }
