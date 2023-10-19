e1bdd2ea4d1e0f28292dafc653ea0e61a7902ee0
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:24:00 +0330

    add OfferDateException file.

---
+++ src/main/java/com/github/javabaz/springexample/exception/OfferDateException.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.exception;
+
+public class OfferDateException extends Exception{
+    public OfferDateException(String message) {
+        super(message);
+    }
+}
