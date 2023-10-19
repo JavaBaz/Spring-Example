4ca56e8532d8bbda3c761c5ab1bcd138d729f689
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:23:28 +0330

    add ImageSizeException file.

---
+++ src/main/java/com/github/javabaz/springexample/exception/ImageSizeException.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.exception;
+
+public class ImageSizeException extends Exception{
+    public ImageSizeException(String message) {
+        super(message);
+    }
+}
