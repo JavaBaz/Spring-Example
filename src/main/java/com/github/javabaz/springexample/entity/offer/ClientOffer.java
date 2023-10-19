2b4b591d22f3a84f08e7bddc5eb4c4ffa33d01f5
JavaBaz <Bazgir.civil@gmail.com>
2023-10-19 18:24:35 +0330

    add ClientOffer.java file.

---
+++ src/main/java/com/github/javabaz/springexample/entity/offer/ClientOffer.java
@@ -0,0 +1,50 @@
+package com.github.javabaz.springexample.entity.offer;
+
+
+import com.github.javabaz.springexample.entity.SubCategory;
+import com.github.javabaz.springexample.entity.enums.OfferStatus;
+import com.github.javabaz.springexample.entity.user.Client;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "client_offer_table")
+@Getter
+@Setter
+@ToString
+public class ClientOffer {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Client client;
+
+    @ManyToOne
+    private SubCategory subCategory;
+
+    private int offeredWage;
+
+    private String description;
+
+    private String address;
+
+    @Column(name = "client_offer_date")
+    @NotNull(message = "Expert offer date can not be null")
+    private LocalDateTime clientOfferDate;
+
+    @Column(name = "client_offer_date_signed")
+    @NotNull(message = "Expert offer date signed can not be null")
+    private LocalDateTime clientOfferDateSigned;
+
+    @Enumerated(EnumType.STRING)
+    private OfferStatus offerStatus;
+
+}
