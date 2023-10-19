98697d18d1e9feeea1fb203aef7c740dd93fdd92
JavaBaz <Bazgir.civil@gmail.com>
2023-10-19 17:58:48 +0330

    add Client.java file.

---
+++ src/main/java/com/github/javabaz/springexample/entity/Client.java
@@ -0,0 +1,24 @@
+package com.github.javabaz.springexample.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "client_table")
+public class Client {
+
+
+
+
+}
