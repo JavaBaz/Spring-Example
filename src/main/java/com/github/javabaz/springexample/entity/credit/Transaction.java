43c943db28737b6b9fa182f6925489b771c7441c
JavaBaz <Bazgir.civil@gmail.com>
2023-10-19 23:00:29 +0330

    add Transaction.java file.

---
+++ src/main/java/com/github/javabaz/springexample/entity/credit/Transaction.java
@@ -0,0 +1,34 @@
+package com.github.javabaz.springexample.entity.credit;
+
+import com.github.javabaz.springexample.entity.user.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "transaction_table")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "amount")
+    private double amount;
+
+    @Column(name = "transaction_date")
+    private LocalDateTime transactionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "expert_id")
+    private Expert expert;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+}
