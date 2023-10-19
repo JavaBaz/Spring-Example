93bcf16aa2556463f54d16659eec46df7056d17d
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:23:48 +0330

    add ImageTypeException file.

---
+++ src/main/java/com/github/javabaz/springexample/exception/ImageTypeException.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.exception;
+
+public class ImageTypeException extends Exception{
+    public ImageTypeException(String message) {
+        super(message);
+    }
+}
