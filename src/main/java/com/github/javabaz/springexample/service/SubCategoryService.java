60ef8a83e31a118aba58985be635fe0a8caacbee
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:30:13 +0330

    add findAll method to SubCategoryService.

--- src/main/java/com/github/javabaz/springexample/service/SubCategoryService.java
+++ src/main/java/com/github/javabaz/springexample/service/SubCategoryService.java
@@ -3,7 +3,11 @@ package com.github.javabaz.springexample.service;
 import com.github.javabaz.springexample.entity.SubCategory;
 import com.github.javabaz.springexample.exception.SubCategoryNotFoundException;
 
+import java.util.List;
+
 public interface SubCategoryService {
 
     SubCategory findByName(String name) throws SubCategoryNotFoundException;
+
+    List<SubCategory> findAll();
 }
