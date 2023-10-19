f0658c5e1df53d80b2d17e79b16299fc5fe8c7df
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 02:25:02 +0330

    refactor CategoryServiceImpl to generic.

--- src/main/java/com/github/javabaz/springexample/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/github/javabaz/springexample/service/impl/CategoryServiceImpl.java
@@ -1,47 +1,17 @@
 package com.github.javabaz.springexample.service.impl;
 
+import com.github.javabaz.springexample.base.service.impl.BaseServiceImpl;
 import com.github.javabaz.springexample.entity.Category;
-import com.github.javabaz.springexample.exception.CategoryNotFoundException;
 import com.github.javabaz.springexample.repository.CategoryRepository;
 import com.github.javabaz.springexample.service.CategoryService;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
-public class CategoryServiceImpl implements CategoryService {
+public class CategoryServiceImpl extends BaseServiceImpl<Category,Long,CategoryRepository> implements CategoryService {
 
-    private final CategoryRepository categoryRepository;
-
-
-    public CategoryServiceImpl(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
-
-
-    @Override
-    public Category findByName(String name) throws CategoryNotFoundException {
-        Category category = categoryRepository.findByName(name);
-        if (category == null) {
-            throw new CategoryNotFoundException("Category not found with name: " + name);
-        }
-        return category;
-    }
-
-    @Override
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
-    }
-
-    @Override
-    public Category saveOrUpdate(Category category) {
-        return categoryRepository.save(category);
-    }
-
-    @Override
-    public void delete(String name) throws CategoryNotFoundException {
-        Category category = findByName(name);
-        categoryRepository.delete(category);
+    public CategoryServiceImpl(CategoryRepository repository) {
+        super(repository);
     }
 
 
