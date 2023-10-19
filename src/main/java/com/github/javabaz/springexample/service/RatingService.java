b35f1b1896e724245641ebe0d1e9972625fc4314
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 02:42:03 +0330

    refactor Rating entity to generic.

---
+++ src/main/java/com/github/javabaz/springexample/service/RatingService.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.service;
+
+import com.github.javabaz.springexample.base.service.BaseService;
+import com.github.javabaz.springexample.entity.Rating;
+
+public interface RatingService extends BaseService<Rating, Long> {
+}
---
+++ src/main/java/com/github/javabaz/springexample/service/impl/RatingServiceImpl.java
@@ -0,0 +1,15 @@
+package com.github.javabaz.springexample.service.impl;
+
+import com.github.javabaz.springexample.base.service.impl.BaseServiceImpl;
+import com.github.javabaz.springexample.entity.Rating;
+import com.github.javabaz.springexample.repository.RatingRepository;
+import com.github.javabaz.springexample.service.RatingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RatingServiceImpl extends BaseServiceImpl<Rating, Long, RatingRepository> implements RatingService {
+
+    public RatingServiceImpl(RatingRepository repository) {
+        super(repository);
+    }
+}
