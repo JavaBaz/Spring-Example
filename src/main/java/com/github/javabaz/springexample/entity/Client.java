0458a99adaceca64fa4529cb4fc6c43f504c1658
JavaBaz <Bazgir.civil@gmail.com>
2023-10-19 17:59:11 +0330

    add fields to Client class.

--- src/main/java/com/github/javabaz/springexample/entity/Client.java
+++ src/main/java/com/github/javabaz/springexample/entity/Client.java
@@ -18,6 +18,35 @@ import java.time.LocalDateTime;
 @Table(name = "client_table")
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    @Column(name = "first_name")
+    @NotNull(message = "First name can not be null")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotNull(message = "Last name can not be null")
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+    @NotNull(message = "Password can not be null")
+    private String password;
+
+    @Column(name = "sign_up_date")
+    @NotNull(message = "sign up date can not be null")
+    private LocalDateTime signUpDate;
+
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "profile_picture")
+    private byte[] profilePicture;
 
 
 
