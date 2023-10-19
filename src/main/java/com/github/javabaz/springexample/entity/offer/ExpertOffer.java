4b89c5075df3ed25052e3ad9d036a674058426f7
JavaBaz <Bazgir.civil@gmail.com>
2023-10-19 18:24:47 +0330

    add ExpertOffer.java file.

---
+++ src/main/java/com/github/javabaz/springexample/entity/offer/ExpertOffer.java
@@ -0,0 +1,45 @@
+package com.github.javabaz.springexample.entity.offer;
+
+import com.github.javabaz.springexample.entity.user.Expert;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "expert_offer_table")
+@Getter
+@Setter
+@ToString
+public class ExpertOffer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    @ManyToOne
+    private ClientOffer clientOffer;
+
+    @ManyToOne
+    private Expert expert;
+
+    @Column(name = "expert_offered_date")
+    private LocalDate expertOfferedDate;
+
+    @Column(name = "days_need_to_work")
+    private int daysNeedToWork;
+
+    @Column(name = "offered_wage")
+    private int offeredWage;
+
+    @Column(name = "expert_offer_date_signed")
+    @NotNull(message = "Expert offer date signed can not be null")
+    private LocalDateTime ExpertOfferDateSigned;
+
+
+}
