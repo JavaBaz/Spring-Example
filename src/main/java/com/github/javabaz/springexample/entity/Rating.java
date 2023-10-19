57a402485592038f2fd42a834e7513278f611ec8
JavaBaz <Bazgir.civil@gmail.com>
2023-10-19 18:23:51 +0330

    refactor imports of Rating class.

--- src/main/java/com/github/javabaz/springexample/entity/Rating.java
+++ src/main/java/com/github/javabaz/springexample/entity/Rating.java
@@ -1,5 +1,7 @@
 package com.github.javabaz.springexample.entity;
 
+import com.github.javabaz.springexample.entity.user.Client;
+import com.github.javabaz.springexample.entity.user.Expert;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
