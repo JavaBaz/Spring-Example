b133f90fe63366714626c3ade6259fd28f7860af
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 03:16:37 +0330

    refactor Wallet entity to generic.

---
+++ src/main/java/com/github/javabaz/springexample/service/impl/WalletServiceImpl.java
@@ -0,0 +1,15 @@
+package com.github.javabaz.springexample.service.impl;
+
+import com.github.javabaz.springexample.base.service.impl.BaseServiceImpl;
+import com.github.javabaz.springexample.entity.credit.Wallet;
+import com.github.javabaz.springexample.repository.WalletRepository;
+import com.github.javabaz.springexample.service.WalletService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletServiceImpl extends BaseServiceImpl<Wallet, Long, WalletRepository> implements WalletService {
+
+    public WalletServiceImpl(WalletRepository repository) {
+        super(repository);
+    }
+}
