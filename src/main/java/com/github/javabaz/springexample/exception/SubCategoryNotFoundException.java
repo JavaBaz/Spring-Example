f40412f10202178e99e75dacba0ba994480208f6
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:24:33 +0330

    add SubCategoryNotFoundException file.

---
+++ src/main/java/com/github/javabaz/springexample/exception/SubCategoryNotFoundException.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.exception;
+
+public class SubCategoryNotFoundException extends Exception{
+    public SubCategoryNotFoundException(String message) {
+        super(message);
+    }
+}
