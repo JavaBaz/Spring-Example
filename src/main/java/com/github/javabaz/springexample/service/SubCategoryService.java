c7541009200ef1ed56867ba59ea6683eb49d8914
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:29:53 +0330

    add findByName method to SubCategoryService.

--- src/main/java/com/github/javabaz/springexample/service/SubCategoryService.java
+++ src/main/java/com/github/javabaz/springexample/service/SubCategoryService.java
@@ -1,4 +1,9 @@
 package com.github.javabaz.springexample.service;
 
+import com.github.javabaz.springexample.entity.SubCategory;
+import com.github.javabaz.springexample.exception.SubCategoryNotFoundException;
+
 public interface SubCategoryService {
+
+    SubCategory findByName(String name) throws SubCategoryNotFoundException;
 }
