3695585cdb2307e4029bb5641f0b797384b1be74
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:24:24 +0330

    add OfferPriceException file.

---
+++ src/main/java/com/github/javabaz/springexample/exception/OfferPriceException.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.exception;
+
+public class OfferPriceException extends Exception{
+    public OfferPriceException(String message) {
+        super(message);
+    }
+}
