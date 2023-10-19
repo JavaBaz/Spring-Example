9b05dc4816345c09ad1cc493ecf38c73ecdc9ddc
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:24:13 +0330

    add OfferNotFoundException file.

---
+++ src/main/java/com/github/javabaz/springexample/exception/OfferNotFoundException.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.exception;
+
+public class OfferNotFoundException extends Exception{
+    public OfferNotFoundException(String message) {
+        super(message);
+    }
+}
