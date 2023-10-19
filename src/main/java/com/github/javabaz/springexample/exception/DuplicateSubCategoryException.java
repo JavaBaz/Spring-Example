bb2fdb04b219ac6e396d461c03f843aedf03619c
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:23:17 +0330

    add DuplicateSubCategoryException file.

---
+++ src/main/java/com/github/javabaz/springexample/exception/DuplicateSubCategoryException.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.exception;
+
+public class DuplicateSubCategoryException extends Exception{
+    public DuplicateSubCategoryException(String message) {
+        super(message);
+    }
+}
