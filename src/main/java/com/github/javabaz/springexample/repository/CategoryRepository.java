79bff4e1fc6d9d531aa74bd53cfa5f2693124bdd
JavaBaz <Bazgir.civil@gmail.com>
2023-10-19 19:14:43 +0330

    add CategoryRepository.java file.

---
+++ src/main/java/com/github/javabaz/springexample/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.repository;
+
+import com.github.javabaz.springexample.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
