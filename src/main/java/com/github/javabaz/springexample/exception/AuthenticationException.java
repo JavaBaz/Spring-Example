eb714646ad6480c081b3480ee62af443e980132d
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 01:22:39 +0330

    add AuthenticationException file.

---
+++ src/main/java/com/github/javabaz/springexample/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.github.javabaz.springexample.exception;
+
+public class AuthenticationException extends Exception{
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
