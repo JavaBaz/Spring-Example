565030de4cf921c4ea0653b07f530a0ac5e90270
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 05:40:12 +0330

    add changing OfferStatus in makeCounterOffer method.

--- src/main/java/com/github/javabaz/springexample/service/impl/ExpertOfferServiceImpl.java
+++ src/main/java/com/github/javabaz/springexample/service/impl/ExpertOfferServiceImpl.java
@@ -1,6 +1,7 @@
 package com.github.javabaz.springexample.service.impl;
 
 import com.github.javabaz.springexample.base.service.impl.BaseServiceImpl;
+import com.github.javabaz.springexample.entity.enums.OfferStatus;
 import com.github.javabaz.springexample.entity.offer.ClientOffer;
 import com.github.javabaz.springexample.entity.offer.ExpertOffer;
 import com.github.javabaz.springexample.entity.user.Expert;
@@ -8,9 +9,11 @@ import com.github.javabaz.springexample.exception.ClientOfferNotAvailableExcepti
 import com.github.javabaz.springexample.exception.OfferDateException;
 import com.github.javabaz.springexample.exception.OfferPriceException;
 import com.github.javabaz.springexample.repository.ExpertOfferRepository;
+import com.github.javabaz.springexample.service.ClientOfferService;
 import com.github.javabaz.springexample.service.ExpertOfferService;
 import com.github.javabaz.springexample.service.ExpertService;
 import com.github.javabaz.springexample.util.Validator;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -20,13 +23,16 @@ import java.util.List;
 public class ExpertOfferServiceImpl extends BaseServiceImpl<ExpertOffer, Long, ExpertOfferRepository> implements ExpertOfferService {
 
     ExpertService expertService;
+    ClientOfferService clientOfferService;
 
-    public ExpertOfferServiceImpl(ExpertOfferRepository repository, ExpertService expertService) {
+    public ExpertOfferServiceImpl(ExpertOfferRepository repository, ExpertService expertService, ClientOfferService clientOfferService) {
         super(repository);
         this.expertService = expertService;
+        this.clientOfferService = clientOfferService;
     }
 
     @Override
+    @Transactional
     public ExpertOffer makeCounterOffer(Expert expert, ClientOffer clientOffer, int counterOfferAmount) throws OfferPriceException, OfferDateException {
 
         List<ClientOffer> availableOffers = expertService.findAvailableOffers(expert);
@@ -54,7 +60,12 @@ public class ExpertOfferServiceImpl extends BaseServiceImpl<ExpertOffer, Long, E
             throw new OfferDateException("ExpertOfferedDate cannot be before the current date and time.");
         }
 
+        ExpertOffer savedExpertOffer = repository.save(expertOffer);
 
-        return repository.save(expertOffer);
+        if (clientOffer.getOfferStatus() == OfferStatus.WAITING_TO_EXPERT_OFFER) {
+            clientOffer.setOfferStatus(OfferStatus.WAITING_TO_CHOOSE_AN_EXPERT);
+            clientOfferService.saveOrUpdate(clientOffer);
+        }
+        return savedExpertOffer;
     }
     }
