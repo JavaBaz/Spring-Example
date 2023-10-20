c18543e72f064495176391925d7a29f9befbd7e4
JavaBaz <Bazgir.civil@gmail.com>
2023-10-20 04:33:51 +0330

    add WAITING_TO_CHOOSE_AN_EXPERT to OfferStatus enum.

--- src/main/java/com/github/javabaz/springexample/entity/enums/OfferStatus.java
+++ src/main/java/com/github/javabaz/springexample/entity/enums/OfferStatus.java
@@ -2,6 +2,7 @@ package com.github.javabaz.springexample.entity.enums;
 
 public enum OfferStatus {
     WAITING_TO_EXPERT_OFFER,
+    WAITING_TO_CHOOSE_AN_EXPERT,
     WAITING_TO_EXPERT_COME,
     START,
     DONE,
